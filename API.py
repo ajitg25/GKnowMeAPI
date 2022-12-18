72f750a9db0733d85251177005606db86d1832d3
Ajit J Gupta <ajitg131@gmail.com>
2022-12-18 23:43:12 +0530

    Update API.py

--- API.py
+++ API.py
@@ -208,4 +208,11 @@ def upload_file():
             return jsonData
 
 if __name__ == "__main__":
+    # Quick test configuration. Please use proper Flask configuration options
+    # in production settings, and use a separate file or environment variables
+    # to manage the secret key!
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
     app.run(debug=True)
